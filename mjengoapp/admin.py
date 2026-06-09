4cf5b2e4d204179139577a17d07fd51a0e04da7f
josephgakono <sirg4kono@gmail.com>
2026-06-09 09:42:34 +0300

    Completed database models and relationships

--- mjengoapp/admin.py
+++ mjengoapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(User)
+admin.site.register(WorkerProfile)
+admin.site.register(CustomerProfile)
+admin.site.register(Job)
+admin.site.register(Quotation)
+admin.site.register(Project)
+admin.site.register(ProgressUpdate)
+admin.site.register(Review)
+admin.site.register(Portfolio)
+admin.site.register(Message)
+admin.site.register(Notification)
 
 # Register your models here.
