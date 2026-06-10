4ab79cb7c14d778677054458c069b09f8dde89e7
josephgakono <sirg4kono@gmail.com>
2026-06-10 23:26:24 +0300

    add jwt authentication

--- mjengoapp/views.py
+++ mjengoapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 from .models import Job, WorkerProfile
 from .serializers import JobSerializer ,WorkerProfileSerializer
 
@@ -7,15 +8,18 @@ from .serializers import JobSerializer ,WorkerProfileSerializer
 class JobListCreateView(generics.ListCreateAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+    permission_classes = [IsAuthenticated]
 
 
 class JobDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Job.objects.all()
     serializer_class = JobSerializer
+    permission_classes = [IsAuthenticated]
 
 class WorkerProfileListCreateView(generics.ListCreateAPIView):
     queryset = WorkerProfile.objects.all()
     serializer_class = WorkerProfileSerializer
+    permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
      serializer.save(user=self.request.user) 
@@ -26,4 +30,4 @@ class WorkerProfileDetailView(
 ):
     queryset = WorkerProfile.objects.all()
     serializer_class = WorkerProfileSerializer
-
+    permission_classes = [IsAuthenticated]
