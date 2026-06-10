626fbcf933c7a50f86a9867ff0158c484e890d98
josephgakono <sirg4kono@gmail.com>
2026-06-10 23:41:00 +0300

    create the remaining core views in views.py

--- mjengoapp/urls.py
+++ mjengoapp/urls.py
@@ -1,9 +1,28 @@
 from django.urls import path
-from .views import JobListCreateView ,JobDetailView,WorkerProfileListCreateView,WorkerProfileDetailView
+from .views import (
+    CustomerProfileDetailView,
+    CustomerProfileListCreateView,
+    JobDetailView,
+    JobListCreateView,
+    MessageDetailView,
+    MessageListCreateView,
+    NotificationDetailView,
+    NotificationListCreateView,
+    PortfolioDetailView,
+    PortfolioListCreateView,
+    ProjectDetailView,
+    ProjectListCreateView,
+    QuotationDetailView,
+    QuotationListCreateView,
+    ReviewDetailView,
+    ReviewListCreateView,
+    WorkerProfileDetailView,
+    WorkerProfileListCreateView,
+)
 
 urlpatterns = [
     path('jobs/', JobListCreateView.as_view(), name='jobs'),
     path('jobs/<int:pk>/', JobDetailView.as_view(), name='job-detail'),
-    path('workerprofile/',WorkerProfileListCreateView.as_view(),name='workerprofile'),
-    path('workerprofile/<int:pk>/',WorkerProfileDetailView.as_view(),name='workerprofile-detail')
+    path('workerprofile/', WorkerProfileListCreateView.as_view(), name='workerprofile'),
+    path('workerprofile/<int:pk>/', WorkerProfileDetailView.as_view(), name='workerprofile-detail'),
 ]
--- mjengoapp/views.py
+++ mjengoapp/views.py
@@ -1,8 +1,27 @@
-from django.shortcuts import render
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import Job, WorkerProfile
-from .serializers import JobSerializer ,WorkerProfileSerializer
+from .models import (
+    CustomerProfile,
+    Job,
+    Message,
+    Notification,
+    Portfolio,
+    Project,
+    Quotation,
+    Review,
+    WorkerProfile,
+)
+from .serializers import (
+    CustomerProfileSerializer,
+    JobSerializer,
+    MessageSerializer,
+    NotificationSerializer,
+    PortfolioSerializer,
+    ProjectSerializer,
+    QuotationSerializer,
+    ReviewSerializer,
+    WorkerProfileSerializer,
+)
 
 
 class JobListCreateView(generics.ListCreateAPIView):
@@ -16,18 +35,101 @@ class JobDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = JobSerializer
     permission_classes = [IsAuthenticated]
 
+
 class WorkerProfileListCreateView(generics.ListCreateAPIView):
     queryset = WorkerProfile.objects.all()
     serializer_class = WorkerProfileSerializer
     permission_classes = [IsAuthenticated]
 
     def perform_create(self, serializer):
-     serializer.save(user=self.request.user) 
+        serializer.save(user=self.request.user)
 
 
-class WorkerProfileDetailView(
-    generics.RetrieveUpdateDestroyAPIView
-):
+class WorkerProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = WorkerProfile.objects.all()
     serializer_class = WorkerProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CustomerProfileListCreateView(generics.ListCreateAPIView):
+    queryset = CustomerProfile.objects.all()
+    serializer_class = CustomerProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class CustomerProfileDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = CustomerProfile.objects.all()
+    serializer_class = CustomerProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class QuotationListCreateView(generics.ListCreateAPIView):
+    queryset = Quotation.objects.all()
+    serializer_class = QuotationSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class QuotationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Quotation.objects.all()
+    serializer_class = QuotationSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ProjectListCreateView(generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ProjectDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ReviewListCreateView(generics.ListCreateAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class ReviewDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class PortfolioListCreateView(generics.ListCreateAPIView):
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class PortfolioDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Portfolio.objects.all()
+    serializer_class = PortfolioSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MessageListCreateView(generics.ListCreateAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class MessageDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Message.objects.all()
+    serializer_class = MessageSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class NotificationListCreateView(generics.ListCreateAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class NotificationDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Notification.objects.all()
+    serializer_class = NotificationSerializer
     permission_classes = [IsAuthenticated]
