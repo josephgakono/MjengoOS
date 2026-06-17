d792e25499ca68d466c5437b1aeedbaa1f7b9dce
josephgakono <sirg4kono@gmail.com>
2026-06-17 17:58:42 +0300

    add nrok url to allowed host

--- mjengoos/settings.py
+++ mjengoos/settings.py
@@ -26,7 +26,12 @@ SECRET_KEY = 'django-insecure-!8nxm^mppxaobgnupmz1dxdthvfd5(=j@j=_%t73#@-+nwrr3s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+    'abrasive-sinless-manifesto.ngrok-free.dev',
+    '.ngrok-free.dev',
+]
 
 
 # Application definition
